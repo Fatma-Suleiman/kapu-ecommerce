b1c8cecc185e9f173a73391d176c7fe46359d6a0
Fatma-Suleiman <97163430+Fatma-Suleiman@users.noreply.github.com>
2025-12-26 11:24:49 +0300

    kapu ecommerce building

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from './pages/HomePage';
+import './App.css'
+import { CheckoutPage } from "./pages/CheckoutPage";
+import {OrdersPage} from './pages/OrdersPage';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={  <HomePage/>}/>
+      <Route path="checkout" element ={<CheckoutPage/>}/>
+      <Route path="orders" element ={<OrdersPage/>}/>
+  </Routes>
+  )
+}
+
+export default App
